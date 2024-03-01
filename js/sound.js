a71fca69551863d2bb7e61d64cc9156f4bca39e3
lainiemorenokhoun <142851769+morenokhoun@users.noreply.github.com>
2024-03-01 18:07:24 +0000

    Fri, Mar 1, 2024, 10:07 AM -08:00

--- js/sound.js
+++ js/sound.js
@@ -4,7 +4,17 @@ sounds.forEach(sound => {
     btn.classList.add('btn')
     btn.innerText=sound
     btn.addEventListener('click', () => {
+        stopSongs()
         document.getElementById(sound).play()
     })
     document.getElementById('buttons').appendChild(btn)
 })
+
+
+function stopSongs(){
+    sounds.forEach(sound => {
+        song = document.getElementById(sound)
+        song.pause()
+        song.currentTime= 0
+    })
+}
