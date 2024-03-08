533907235387c51fee1b626572e7f266cb82132b
lainiemorenokhoun <142851769+morenokhoun@users.noreply.github.com>
2024-03-08 10:51:07 -0800

    LandingPage

---
+++ js/landingPage.js
@@ -0,0 +1,27 @@
+toggle = document.getElementById('toggle')
+open = document.getElementById('open')
+modal = document.getElementById('modal')
+close = document.getElementByIs('close')
+
+// Toggle Nav
+toggle.addEventListener('click', () => {
+    document.body.classList.toggle('show-nav')
+})
+
+// Show Modal
+open.addEventListener('click', () => {
+    modal.classList.add('show-modal')
+})
+
+// Hide Modal
+close.addEventListener('click', () => {
+    modal.classList.remove('show-modal')
+})
+
+// Hide Model on outside click
+window.addEventListener('click', (e) => {
+    if (e.target == modal) {
+        modal.classList.remove('show-modal')
+    }
+}
+)
